212f8c7c6e93c35e7eb54ddab9495cfd5173d07d
Kapil Yadav <yadkapil@amazon.com>
2016-09-11 19:56:06 +0530

    channel test added

--- app/src/main/java/com/jedi/tictactoe/TicTacToeActivity.java
+++ app/src/main/java/com/jedi/tictactoe/TicTacToeActivity.java
@@ -10,4 +10,8 @@ public class TicTacToeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tic_tac_toe);
     }
+
+    private int getMaxScore(){
+        return 0;
+    }
 }
